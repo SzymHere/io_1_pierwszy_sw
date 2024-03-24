33311df238d782003667fb0312817c3b68813e49
Szymon Winiarski <93779@student.ue.poznan.pl>
2024-03-24 11:00:53 +0100

    new commit2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Trójkąt
 def triangle_area(base, height):
     """
     Calculates the area of a triangle given its base and height.
@@ -18,3 +19,23 @@ height = float(input("Enter the height of the triangle: "))
 
 area = triangle_area(base, height)
 print("The area of the triangle is:", area)
+
+# Kwadrat
+def square_area(side_length):
+    """
+    Calculates the area of a square given the length of its side.
+    
+    Args:
+    - side_length (float): The length of a side of the square.
+    
+    Returns:
+    - float: The area of the square.
+    """
+    area_sq = side_length * side_length
+    return area_sq
+
+# Example usage:
+side_length = float(input("Enter the length of a side of the square: "))
+
+area_sq = square_area(side_length)
+print("The area of the square is:", area_sq)
